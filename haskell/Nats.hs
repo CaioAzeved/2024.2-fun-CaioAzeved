0be408e9ff0630c58f2801ce9c795829f8e99fcd
Caio Vinicius <caiocvam3@gmail.com>
2024-10-10 20:06:52 -0300

    Implementação do times

--- haskell/Nats.hs
+++ haskell/Nats.hs
@@ -8,8 +8,15 @@
          deriving (Eq, Show)
 
 
+    --operations
+
     plus :: Nat -> Nat -> Nat
 
     plus n O = n
     plus n (S m) = S (plus n m)
+
+    times :: Nat -> Nat -> Nat
+
+    times _ O = O
+    times n (S m) = plus n (times n m)
 
