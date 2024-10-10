970d6f9be0b338e74adc4f2ffd25b44774b63173
Caio Vinicius <caiocvam3@gmail.com>
2024-10-10 20:13:42 -0300

    Adição de uns carinhas legais :)

--- haskell/Nats.hs
+++ haskell/Nats.hs
@@ -7,6 +7,19 @@
         S :: Nat -> Nat
          deriving (Eq, Show)
 
+    --useful sugars
+
+    o, so, sso, ssso, sssso, ssssso, sssssso, ssssssso, sssssssso, ssssssssso :: Nat
+    o    = O
+    so   = S o
+    sso  = S so
+    ssso = S sso
+    sssso = S ssso
+    ssssso = S sssso
+    sssssso = S ssssso
+    ssssssso = S sssssso
+    sssssssso = S ssssssso
+    ssssssssso = S sssssssso
 
     --operations
 
