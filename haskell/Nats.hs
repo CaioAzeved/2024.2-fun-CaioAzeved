33e037a4f56ff9e587fe4118aa880afa6330562a
Caio Vinicius <caiocvam3@gmail.com>
2024-10-11 21:39:33 -0300

    Organizando mais o código

--- haskell/Nats.hs
+++ haskell/Nats.hs
@@ -24,22 +24,18 @@
     --operations
 
     plus :: Nat -> Nat -> Nat
-
     plus n O = n
     plus n (S m) = S (plus n m)
 
     times :: Nat -> Nat -> Nat
-
     times _ O = O
     times n (S m) = plus n (times n m)
 
     exp :: Nat -> Nat -> Nat
-
     exp _ O = so
     exp n (S m) = times n (exp n m)
 
     sub :: Nat -> Nat -> Nat
-
     sub n O = n
     sub (S n) (S m) = sub n m
     sub O _ = O  
