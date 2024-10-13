ceca7824a4cf50217c3db23c1652dc6961fbecc6
Caio Vinicius <caiocvam3@gmail.com>
2024-10-13 20:43:49 -0300

    Sim, eu coloquei o tipo WeekDay

---
+++ haskell/WeekDay.hs
@@ -0,0 +1,12 @@
+    module WeekDay where
+    
+    data WeekDay where
+        Sunday :: WeekDay
+        Monday :: WeekDay
+        Tuesday :: WeekDay
+        Wednesday :: WeekDay
+        Thursday :: WeekDay
+        Friday :: WeekDay
+        Saturday :: WeekDay
+         deriving(Eq, Show)
+
