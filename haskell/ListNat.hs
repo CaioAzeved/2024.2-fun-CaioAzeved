a310bd7b902f8604d060125948a25ec9905a6813
Caio Vinicius <caiocvam3@gmail.com>
2024-10-13 15:12:20 -0300

    lenght implementada

--- haskell/ListNat.hs
+++ haskell/ListNat.hs
@@ -1,11 +1,16 @@
-module ListNat where
+    module ListNat where
 
-import Prelude (Eq, Show)
-import Nat
-import Bool
-import Function
+    import Prelude (Eq, Show)
+    import Nat
+    import Bool
+    import Function
 
-data ListNat where
-    Nil :: ListNat
-    Cons  :: Nat -> ListNat -> ListNat
-  deriving (Eq, Show)
+    data ListNat where
+        Nil :: ListNat
+        Cons :: Nat -> ListNat -> ListNat
+         deriving (Eq, Show)
+
+    lenght :: ListNat -> Nat
+
+    lenght Nil = O
+    lenght (_ `Cons` xs) = S (lenght xs)
