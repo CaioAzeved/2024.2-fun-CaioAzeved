969b6c201d32837dbefb41fe3271b3550ca002b2
Caio Vinicius <caiocvam3@gmail.com>
2024-10-25 10:24:02 -0300

    Implementada a filter

--- haskell/hw3/ExList.hs
+++ haskell/hw3/ExList.hs
@@ -177,7 +177,13 @@ inits (x : xs) = (inits $ init $ x : xs) ++ [x : xs]
 
 -- (!!)
 
--- filter
+filter :: (a -> Bool) -> [a] -> [a]
+filter p [] = []
+filter p (x: xs) = 
+  if p x
+  then x : (filter p xs)
+  else filter p xs
+
 -- map
 
 -- cycle
